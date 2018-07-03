82bf687cc7f0b78f5c5ccd49e1d0cce123ef0f82
Fr4nZ82 <35651871+Fr4nZ82@users.noreply.github.com>
2018-07-03 21:52:16 +0200

    hewo

--- hewo.js
+++ hewo.js
@@ -7,6 +7,4 @@ var server = http.createServer(function (req, res) {
  
 server.listen(1337, '127.0.0.1');
 
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-
 console.log('Server running at http://127.0.0.1:1337/');
